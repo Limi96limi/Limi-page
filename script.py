1593ac1317dd68cec69310c6a4b87ccce1909c50
Limi96limi <106404810+Limi96limi@users.noreply.github.com>
2023-09-07 18:32:56 +0200

    added stuff
    added lot of files

---
+++ script.py
@@ -0,0 +1,11 @@
+#Get input from the user
+num1 = int(input("Enter the fniggairst number: "))
+num2 = int(input("Enter the second number: "))
+num3 = int(input("Enter the third number: "))
+num4 = int(input("Enter the forth number: "))
+
+#Calculate the sum
+sum_result = (num1 ** num2 + num3) - num4
+
+#Display the result
+print(f"The sum of {num1}, {num2} and {num3} is {sum_result}")
